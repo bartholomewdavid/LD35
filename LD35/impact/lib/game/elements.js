ccdaf80cdb3e17afd8a3f145d4a849fab419b3d4
Eric Valadas <ericvaladas@gmail.com>
2016-04-16 17:24:47 -0400

    Added element multipliers for damage

--- LD35/impact/lib/game/elements.js
+++ LD35/impact/lib/game/elements.js
@@ -8,5 +8,32 @@ ig.module(
         WIND: 'Wind',
         FIRE: 'Fire',
         WATER: 'Water'
+    },
+
+    ElementMultiplier = {
+        'Earth': {
+            'Earth': 1.00,
+            'Wind': 0.50,
+            'Fire': 1.50,
+            'Water': 2.00
+        },
+        'Wind': {
+            'Earth': 2.00,
+            'Wind': 1.00,
+            'Fire': 0.50,
+            'Water': 1.50
+        },
+        'Water': {
+            'Earth': 0.50,
+            'Wind': 1.50,
+            'Fire': 2.00,
+            'Water': 1.00
+        },
+        'Fire': {
+            'Earth': 1.50,
+            'Wind': 2.00,
+            'Fire': 1.00,
+            'Water': 0.50
+        }
     }
 });
